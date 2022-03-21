c27f5dbe95d154e133d85adfabfdbc57cb98e8fe
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-03-21 17:39:05 -0300

    criação parcial do app swing.

--- src/br/com/java/swing/projetos/GestaoEstudantil.java
+++ src/br/com/java/swing/projetos/GestaoEstudantil.java
@@ -1,24 +1,54 @@
 package br.com.java.swing.projetos;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.border.EmptyBorder;
+import java.awt.EventQueue;
 
 public class GestaoEstudantil extends JFrame{
     
     private static final long serialVersionUID = -8360585084201808315L;
     private JPanel conteudoPainel;
     private JPanel principalPainel;
-    private JPanel painel;
+    //private JPanel painel;
 
-    private JLabel lblNome;
-    private JTextField txtNome;
+    //private JLabel lblNome;
+    //private JTextField txtNome;
 
-    private JLabel lblChamadaNo;
+    //private JLabel lblChamadaNo;
+
+	private JLabel ativoUsuario;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
-            run
+            @Override
+			public void run() {
+				try {
+					GestaoEstudantil frame = new GestaoEstudantil();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+			}
         });
     }
+	//public GestaoEstudantil(String ativoUsuario) {
+	//	this();
+	//	this.getAtivoUsuario().setText(ativoUsuario);
+	//}
+	public GestaoEstudantil() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 771, 538);
+		conteudoPainel = new JPanel();
+		conteudoPainel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(conteudoPainel);
+		conteudoPainel.setLayout(null);
+		conteudoPainel.add(getPrincipalPainel());
+	}
+	private JPanel getPrincipalPainel() {
+		return principalPainel;	
+	}
+	private JLabel getAtivoUsuario() {
+		return ativoUsuario;
+	}
 }
